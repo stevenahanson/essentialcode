bc71b08a56229b41d2971ec5deeacb564ea8ff9c
Steven Hanson <stevenahanson@live.com>
2023-10-24 20:18:56 +0100

    Move userProfile into own component

--- src/app/components/UserProfile.tsx
+++ src/app/components/UserProfile.tsx
@@ -1,3 +1,18 @@
 export default function UserProfile() {
-  return "Hi from user profile";
+  const userProfile = {
+    userName: "SHanson",
+    fullName: "Steven Hanson",
+    followers: 1000000,
+    following: 2,
+  };
+
+  const { userName, followers, following } = userProfile;
+
+  return (
+    <div>
+      <p>Username: {userName}</p>
+      <p>Following: {following}</p>
+      <p>Followers: {followers}</p>
+    </div>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,15 @@
 import UserProfile from "./components/UserProfile";
 
 export default function Home() {
-  const userProfile = {
-    userName: "SHanson",
-    fullName: "Steven Hanson",
-    followers: 1000000,
-    following: 2,
-  };
-
-  const { userName, followers, following } = userProfile;
-
   return (
     <>
-      <UserProfile />
       <div>
         <p>Profile</p>
       </div>
       <div>
         <h1>Your profile:</h1>
       </div>
-      <div>
-        <p>Username: {userName}</p>
-        <p>Following: {following}</p>
-        <p>Followers: {followers}</p>
-      </div>
+      <UserProfile />
     </>
   );
 }
