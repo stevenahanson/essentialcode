669978893318419f863778b4000ce989f5596ab0
Steven Hanson <stevenahanson@live.com>
2023-10-24 20:09:55 +0100

    destructuring mentors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,17 @@
 export default function Home() {
-  return <p>Hello World</p>;
+  const mentors = ["leonardo dicaprio", "steven barlett", "master p"];
+
+  const [third, first, second] = mentors;
+
+  console.log("current mentor is " + first);
+  console.log("future mentor is " + second);
+  console.log("super future mentor is " + third);
+
+  return (
+    <div>
+      <p>I am currently being mentored by: {first}</p>
+      <p>In the future, {second} will be my mentor</p>
+      <p>In the super future, {third} will be my mentor</p>
+    </div>
+  );
 }
